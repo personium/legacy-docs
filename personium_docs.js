c2166f7270f2953ea50e2a62d78ae6dc3326a262
Akio Shimono <shimono@users.noreply.github.com>
2018-02-02 21:14:36 +0900

    Update personium_docs.js

--- personium_docs.js
+++ personium_docs.js
@@ -1,6 +1,6 @@
 var path = document.location.pathname.split('/');
-var pathCssCommon = [ path[0], "personium.css" ];
-var pathCssLocale = [ path[0], path[1], "locale.css" ];
+var pathCssCommon = [ path[0], path[1], "personium.css" ];
+var pathCssLocale = [ path[0], path[1], path[2], "locale.css" ];
 
 document.write('<link rel="stylesheet" href="' + pathCssCommon.join('/') + '" type="text/css" />');
 document.write('<link rel="stylesheet" href="' + pathCssLocale.join('/') + '" type="text/css" />');
