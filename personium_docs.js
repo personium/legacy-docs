1e331a246119355ed19e3c3a61251b1eb5462a39
Akio Shimono <shimono@users.noreply.github.com>
2018-02-02 21:19:48 +0900

    Update personium_docs.js

--- personium_docs.js
+++ personium_docs.js
@@ -1,7 +1,11 @@
-var path = document.location.pathname.split('/');
+var path = location.pathname.split('/');
 var pathCssCommon = [ path[0], path[1], "personium.css" ];
 var pathCssLocale = [ path[0], path[1], path[2], "locale.css" ];
 
 document.write('<link rel="stylesheet" href="' + pathCssCommon.join('/') + '" type="text/css" />');
 document.write('<link rel="stylesheet" href="' + pathCssLocale.join('/') + '" type="text/css" />');
 
+var setLang = function(lang) {
+  path[2] = lang;
+  location.href = path.join('/'); 
+};
