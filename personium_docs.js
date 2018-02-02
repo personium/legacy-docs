ca720a0a88503ab51a5581fd8ab9cc5f5f1ead62
Akio Shimono <shimono@users.noreply.github.com>
2018-02-03 08:57:49 +0900

    Update personium_docs.js

--- personium_docs.js
+++ personium_docs.js
@@ -1,20 +1,18 @@
-var path = location.pathname.split('/');
-var pathCssCommon = [ path[0], path[1], "personium.css" ];
-var pathCssLocale = [ path[0], path[1], path[2], "locale.css" ];
-
-document.write('<link rel="stylesheet" href="' + pathCssCommon.join('/') + '" type="text/css" />');
-document.write('<link rel="stylesheet" href="' + pathCssLocale.join('/') + '" type="text/css" />');
 
 var setLang = function(lang) {
+  var path = location.pathname.split('/');
   path[2] = lang;
   location.href = path.join('/'); 
 };
 
 document.addEventListener('DOMContentLoaded', function() {
+  // set the document title, using the content of h1 tag
   var h1 = document.getElementsByTagName('h1');
+  // Default Documents if not a single h1 tag does not exist. 
   var t = 'Documents';
   if (h1.length == 1) {
     t = h1[0].innerText;
   }
+  // Adding Prefix.
   document.title = t + ' - Personium';
 })
